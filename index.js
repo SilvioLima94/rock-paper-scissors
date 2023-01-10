981c7021e49220cd0d97c52b03f06ca3d5380f7f
Silvio Pina <silviopina94@gmail.com>
2023-01-10 02:20:31 -0500

    added a game func that will allow user to run multiple game rounds

--- index.js
+++ index.js
@@ -9,22 +9,41 @@ function getComputerChoice() {
 // console.log(getComputerChoice());
 
 // play a round of game
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection = "", computerSelection = "") {
   const userSelection = playerSelection.toLowerCase();
   const cpuSelection = computerSelection.toLowerCase();
+
+  let cpuScore = 0;
+  let userScore = 0;
+
   if (cpuSelection === "paper" && userSelection) {
+    cpuScore++;
     console.log(`cpu: ${cpuSelection}, user: ${userSelection}`);
-    return "You Lose! Paper beats Rock";
+    console.log("cpuScore: " + cpuScore);
+    return cpuScore;
   } else if (cpuSelection === "scissors" && userSelection) {
+    userScore++;
     console.log(`cpu: ${cpuSelection}, user: ${userSelection}`);
-    return "You Win! Rock beats Scissors";
+    console.log("userScore: " + userScore);
+    return userScore;
   } else {
     cpuSelection === userSelection;
     console.log(`cpu: ${cpuSelection}, user: ${userSelection}`);
     return "It's a tie";
   }
+  //   cpuScore > userScore
+  //     ? "The winner is " + cpuScore
+  //     : "The loser is " + userScore;
+}
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    playRound(playerSelection, computerSelection);
+    console.log(playerSelection, computerSelection);
+  }
 }
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
+console.log(game());
